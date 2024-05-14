5ef7ba61e1296d118f98cebee3284c840c7ae5f1
leandjb <leandjb@gmail.com>
2024-05-13 19:07:25 -0500

    added new testcase

--- cypress/e2e/3-free-range-testers/homepage.spec.cy.js
+++ cypress/e2e/3-free-range-testers/homepage.spec.cy.js
@@ -6,12 +6,13 @@ describe('Home de freerangetesters.com', () => {
 
     })
 
-    it('shoul hav a title', ()=>{
+    it('should have a title of freerangetesters', () => {
         cy.title().should('include', 'Free Range Testers');
+    })
+
+    it('verify intro message', ()=>{
         cy.get('[data-testid="header-container"] > .sc-gHjVMF > .sc-eTNRI > :nth-child(2) > .sc-dJGMql').click();
-        cy.get(':nth-child(1) > .sc-cSxRuM > .sc-jMbVJB > .sc-iCKXBC > .sc-iKOmoZ > .sc-dJGMql > .sc-gLLuof').click();
-
-
+        cy.get('[data-react-component="creator_ui/section_adapters/Image"] > .sc-dmyCSP > [data-testid="container"] > .sc-cSxRuM > .sc-jMbVJB > .sc-iCKXBC > .sc-iKOmoZ > .sc-gLLuof').should('exist');
     })
 
 })
